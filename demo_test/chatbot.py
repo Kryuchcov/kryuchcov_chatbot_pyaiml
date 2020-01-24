e4b7eee66be3eba998cd83089f40f96bc6c86e1b
kryuchcov <kryuchcov@gmail.com>
2020-01-24 16:49:28 -0600

    Created all files to run a basi demo chatbot

---
+++ demo_test/chatbot.py
@@ -0,0 +1,21 @@
+import os
+import aiml
+
+#Create the kernel and learn AIML files or load a brain file
+kernel=aiml.Kernel()
+if os.path.isfile('aiml_files/bot_brain.brn'):
+    kernel.bootstrap(brainFile='aiml_files/bot_brain.brn')
+else:
+    kernel.bootstrap(learnFiles='std-startup.xml',commands='load aiml b')
+    kernel.saveBrain('aiml_files/bot_brain.brn')
+
+#Press CTRL-C to break this loop
+while True:
+    message=input('Enter your message to the bot: ')
+    if message=='quit':
+        exit()
+    elif message=='save':
+        kernel.saveBrain('bot_brain.brn')
+    else:
+        bot_response=kernel.respond(message)
+        #Do something with bot_response
